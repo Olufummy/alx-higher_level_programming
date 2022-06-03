ae997ce38e75fa799b5955ec3ceff469f0dd8a7e
Olufummy <Adeboyejoisaac18@gmail.com>
2022-06-04 05:28:04 +0800

    update 0x02 import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+
 if __name__ == "__main__":
-    length = len(argv)
-    count = 0
-    if length == 1:
-        print("{}".format(count))
-    else:
-        for i in range(1, length):
-            count = count + int(argv[i])
-        print(count)
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import variable_load_5
-    print("{}".format(variable_load_5.a))
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
